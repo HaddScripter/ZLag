d34d726c7ca6f7789a466f79cf00ff42d193521b
HaddScripter <insanomohamed@gmail.com>
2026-01-05 00:40:44 +0100

    Create Hadda0u1LagTest.lua with GUI and services
    Initialize Hadda0u1LagTest.lua with GUI setup and services.

---
+++ Hadda0u1LagTest.lua
@@ -0,0 +1,35 @@
+local Players =
+
+game:GetService("Players")
+
+local RunService =
+
+game:GetService("RunService")
+
+local StatsService =
+
+game:GetService("Stats")
+
+local UserInputService =
+
+game:GetService("UserInputService")
+
+local CoreGui =
+
+game:GetService("CoreGui")
+
+local TweenService =
+
+game:GetService("TweenService")
+
+local LocalPlayer = Players.LocalPlayer
+
+local ScreenGui =
+
+Instance.new("ScreenGui")
+
+ScreenGui.Name = "ZzzzLagGUI"
+
+ScreenGui. ResetOnSpawn = false
+
+if syn and syn.protect_gui then syn.protect_gui(ScreenGui)
